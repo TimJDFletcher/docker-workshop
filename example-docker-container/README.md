0dd73730064658acfa631ef69ddf64cf99c2a228
Tim Fletcher <tim@night-shade.org.uk>
2018-04-02 15:48:16 +0100

    Import an example container

---
+++ example-docker-container/README.md
@@ -0,0 +1,3 @@
+# example-docker-container
+
+This is a set of examples for how to build a Docker container.
---
+++ example-docker-container/hello.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+case $1 in 
+    -hello)
+        echo Hello World
+        exit 0
+    ;;
+    -test)
+        if [ -x /usr/bin/python ] ; then 
+            echo Python is installed
+            exit 0
+        else
+            echo Python is not installed
+            exit 1
+        fi
+    ;;
+    *)
+        echo call with -hello or -test
+    ;;
+esac
