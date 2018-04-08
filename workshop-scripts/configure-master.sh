f7096c3da8bbcd37bab9f377e44913708805a199
Tim Fletcher <tim@night-shade.org.uk>
2018-04-08 21:26:25 +0200

    Correct the contents of /etc/host for the master nodes

--- workshop-scripts/configure-master.sh
+++ workshop-scripts/configure-master.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -e
-sudo hostnamectl set-hostname rancher-server
+my_ip="$(ip route get 8.8.8.8| grep -oP '(?<=src ).*')"
+hostname=rancher-server
+
+set_hostname()
+{
+    echo ${my_ip}       ${hostname} | sudo tee --append /etc/hosts > /dev/null
+    sudo hostnamectl set-hostname ${hostname}
+}
+
+set_hostname
 sudo docker run -d --restart=unless-stopped -p 8080:8080 rancher/server
