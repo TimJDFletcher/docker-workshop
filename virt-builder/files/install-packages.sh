6f6df5c074bbdf978c6696d00fd4cc218eeadec4
Tim Fletcher <tim@night-shade.org.uk>
2018-04-14 18:22:50 +0200

    Import renamed file

---
+++ virt-builder/files/install-packages.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -e
+DEBIAN_FRONTEND=noninteractive
+APT="apt-get --yes --no-install-recommends"
+PACKAGES="docker-ce"
+count=0
+
+while ! ip route get 8.8.8.8 ; do 
+    sleep 3
+    count=$((count+1))
+    [ $count -gt 10 ] && break
+done
+
+$APT update
+$APT dist-upgrade
+$APT install $PACKAGES
+$APT autoremove
