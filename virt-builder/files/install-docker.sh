e0c5973a89f5cc58fd2d47a0e3c25dfc4bd98d3e
Tim Fletcher <tim@night-shade.org.uk>
2018-04-14 16:59:45 +0200

    Add a test for networking

--- virt-builder/files/install-docker.sh
+++ virt-builder/files/install-docker.sh
@@ -1,6 +1,13 @@
 #!/bin/sh -e
 DEBIAN_FRONTEND=noninteractive
 APT="apt-get --yes -qq"
+count=0
+
+while ! ip route get 8.8.8.8 ; do 
+    sleep 3
+    count=$((count+1))
+    [ $count -gt 10 ] && break
+do
 $APT update
 $APT dist-upgrade
 $APT install docker-ce
