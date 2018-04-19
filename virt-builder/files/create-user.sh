13e1035ac4f22549b073eb9cf3fc0041c8e5ccbd
Tim Fletcher <tim@night-shade.org.uk>
2018-04-20 00:24:42 +0200

    Change script to loop over groups to avoid breaking without network

--- virt-builder/files/create-user.sh
+++ virt-builder/files/create-user.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 password=Docker2018
 user=admin
+groups="sudo adm docker"
 
-useradd -s /bin/bash -m -G adm,sudo,docker -p "" $user
+useradd -s /bin/bash -m -p "" $user
 echo ${user}:${password} | chpasswd
+
+for group in $groups ; do
+    usermod $user -G $group -a
+done
